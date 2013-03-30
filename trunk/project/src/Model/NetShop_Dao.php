5636e563c08db88c78c21f5410f8d90e37e174ca
bertalan.verebelyi@gmail.com <bertalan.verebelyi@gmail.com@8c8f8b9a-9680-e8bb-6d29-ba458c0c4fac>
2013-03-30 20:45:25 +0000

    NetShop_Dao.php uploaded.

---
+++ trunk/project/src/Model/NetShop_Dao.php
@@ -0,0 +1,23 @@
+<?php
+	class NetShop_Dao
+	{
+		private $FELHASZNALONEV = 'h070183';
+		private $JELSZO = 'qwerty';
+		private $HOST = 'xe';
+		private $_connect;
+		
+		public function __construct()
+		{
+			$this->connect = oci_connect($this->FELHASZNALONEV, $this->JELSZO, $this->HOST);
+			
+			if (!$this->connect) {
+				$e = oci_error();
+				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
+		}
+		
+		public function __destruct()
+		{
+			oci_close($this->connect);
+		}
+	}
