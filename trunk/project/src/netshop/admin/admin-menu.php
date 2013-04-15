627adb33024ac3beefd9bedc600771670445b747
csaba.verebelyi@gmail.com <csaba.verebelyi@gmail.com@8c8f8b9a-9680-e8bb-6d29-ba458c0c4fac>
2013-04-15 20:22:55 +0000

    admin menüszerkezet és felhasználó kilistázás implementálva

---
+++ trunk/project/src/netshop/admin/admin-menu.php
@@ -0,0 +1,70 @@
+	<header class="title-head">
+                <h1 class="cim pull-left"><a rel="external" href="index.php">NetShop</a></h1>
+
+                <div id="kijelentkezes" class="pull-right">
+                    <a href="logout.php">Kijelentkezés</a>
+                </div>
+
+                <br class="clearfix" />
+
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="product.php">Termék feltöltése</a>
+                        </li>
+                        <li>
+                            <a href="product-edit.php">Termék módosítása</a>
+                        </li>
+                        <li>
+                            <a href="category.php">Kategória felvétele</a>
+                        </li>
+                        <li>
+                            <a href="category-edit.php">Kategória módosítása</a>
+                        </li>
+                        <li>
+                            <a href="catalog.php">Katalógus megtekintése</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <br class="clearfix" />
+
+            <div id="side" class="sidebars pull-left">
+                <h3>Menü</h3>
+                <nav>
+                    <ul>
+                        <li class="menu-head">
+                            Profil Beállítások
+                        </li>
+                        <li>
+                            <ul class="inner">
+                                <li>
+                                    <a href="logout.php">Kijelentkezés</a>
+                                </li>
+                                <li>
+                                    <a href="update.php">Adatok módosítása</a>
+                                </li>
+                                <li>
+                                    <a href="delete.php">Regisztráció törlése</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-head">
+                            Admin funkciók
+                        </li>
+                        <li>
+                            <ul class="inner">
+                                <li>
+                                    <a href="#">Éves statisztika</a>
+                                </li>
+                                <li>
+                                    <a href="#">Vásárlások listázása</a>
+                                </li>
+                                <li>
+                                    <a href="#">Felhasználók kezelése</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
--- trunk/project/src/netshop/admin/update.php
+++ trunk/project/src/netshop/admin/update.php
@@ -26,77 +26,8 @@
 	    }
     ?>
         <div id="wrapper">
-            <header class="title-head">
-                <h1 class="cim pull-left"><a rel="external" href="index.html">NetShop</a></h1>
-
-                <div id="kijelentkezes">
-                	<a href="delete.php">Felhasználó törlése</a>
-                    <a href="logout.php">Kijelentkezés</a>
-                </div>
-
-                <br class="clearfix" />
-
-                <nav>
-                    <ul>
-                        <li>
-                            <a rel="external" href="#">Termék feltöltése</a>
-                        </li>
-                        <li>
-                            <a rel="external" href="#">Termék módosítása</a>
-                        </li>
-                        <li>
-                            <a rel="external" href="#">Kategória felvétele</a>
-                        </li>
-                        <li>
-                            <a rel="external" href="#">Kategória módosítása</a>
-                        </li>
-                        <li>
-                            <a rel="external" href="#">Katalógus megtekintése</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <br class="clearfix" />
-
-            <div id="side" class="sidebars pull-left">
-                <h3>Menü</h3>
-                <nav>
-                    <ul>
-                        <li class="menu-head">
-                            Profil Beállítások
-                        </li>
-                        <li>
-                            <ul class="inner">
-                                <li>
-                                    <a href="#">Kijelentkezés</a>
-                                </li>
-                                <li>
-                                    <a href="#">Adatok módosítása</a>
-                                </li>
-                                <li>
-                                    <a href="#">Regisztráció törlése</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="menu-head">
-                            Admin funkciók
-                        </li>
-                        <li>
-                            <ul class="inner">
-                                <li>
-                                    <a href="#">Éves statisztika</a>
-                                </li>
-                                <li>
-                                    <a href="#">Vásárlások listázása</a>
-                                </li>
-                                <li>
-                                    <a href="#">Felhasználók kezelése</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            
+            <?php require_once "admin-menu.php" ?>
 
             <div id="core" class="profile pull-left">
                 <h2 class="pull-center">Profil módosítások</h2>
