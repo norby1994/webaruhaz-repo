991cd4c1f6a8467a444fc929fa98856bc1ea5576
szintia777 <szintia777@gmail.com>
2013-04-27 16:07:45 +0000

    Admin felulet - vasarlasok listazasa megvalositas

--- project/src/netshop/admin/orders.php
+++ project/src/netshop/admin/orders.php
@@ -1,8 +1,35 @@
+<!doctype html>
+<html lang="hu">
+    <head>
+        <meta charset="utf-8">
+        <title>NetShop - Admin Bejelentkezés</title>
+        <meta name="description" content="Internetes áruház" />
+        <meta name="author" content="Kasziba Szintia, Verebélyi Bertalan, Verebélyi Csaba" />
+        <link rel="shortcut icon" href="../img/favicon.png" />
+        <link rel="stylesheet" href="../css/reset.css" />
+        <link rel="stylesheet" href="../css/style.css" />
+
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')</script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+    <header class="title-head">
+                <h1 class="cim pull-left"><a rel="external" href="index.php">NetShop</a></h1>
+               
+     </header>
+     <div id="core" class="profile pull-left">
+                <h2 class="pull-center">Vásárlások listázása</h2>
+                
 <?php
 	require_once "../php/admin.php";
-	session_check();
+	list_rendeles();
 ?>
-
-<?php
-	include "../footer.php";
-?>
+</div>
+<script type="text/javascript" src="js/scripts.js"></script>
+        <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+</body>
+</html>
