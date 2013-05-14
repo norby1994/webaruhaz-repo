664b65f122befaee8b84ca7fa942b56be935b4f3
bertalan.verebelyi@gmail.com <bertalan.verebelyi@gmail.com@8c8f8b9a-9680-e8bb-6d29-ba458c0c4fac>
2013-05-14 12:17:19 +0000

    category_view.php

---
+++ trunk/project/src/netshop/category_view.php
@@ -0,0 +1,51 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="hu">
+	<head>
+		<meta charset="utf-8">
+		<title>NetShop</title>
+		<meta name="description" content="Internetes áruház" />
+		<meta name="author" content="Kasziba Szintia, Verebélyi Bertalan, Verebélyi Csaba" />
+		<link rel="shortcut icon" href="img/favicon.png" />
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/style.css" />
+
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')</script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<header class="title-head">
+				<h1 class="cim pull-left"><a rel="external" href="index.php">NetShop</a></h1>
+
+				<?php
+	require_once '/template/bejelentkezes-menu.php';
+ ?>
+
+				<br class="clearfix" />
+
+				<nav>
+					<ul>
+						<?php
+						require_once 'categories.php';
+						category_menu();
+ ?>
+					</ul>
+				</nav>
+			</header>
+			<br class="clearfix" />
+
+			<div id="core" class="landing pull-left">
+				<?php require_once '/php/categories.php'; ?>
+				<h2>Üdvözlünk a NetShop oldalán!</h2>
+				<?php category_view(); ?>
+				
+			</div>
+
+			<?php
+	include "footer.php";
+ ?>
