f762460c470f601d24d42b8f4b37070c6fad9a60
bertalan.verebelyi <bertalan.verebelyi@gmail.com>
2013-05-14 12:14:37 +0000

    kategória termékek listázása

--- project/src/netshop/php/categories.php
+++ project/src/netshop/php/categories.php
@@ -20,9 +20,35 @@ function category_menu() {
 		$e = oci_error($stid);
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 	}
-	
+
 	while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
 		echo '<li><a href="category_view.php?id=' . $row['KATEGORIA_ID'] . '">' . $row['KATEGORIA_NEV'] . '</a></li>';
+	}
+
+	oci_free_statement($stid);
+	oci_close($connect);
+}
+
+/**
+ * kategory view-ban a kategóriához
+ * tartozó termékek kilistázása
+ */
+function category_view($id) {
+	require_once "connection.php";
+	$stid = oci_parse($connect, 'SELECT * FROM termek WHERE kategoria_id = $id');
+	if (!$stid) {
+		$e = oci_error($connect);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	$r = oci_execute($stid);
+	if (!$r) {
+		$e = oci_error($stid);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+		echo $row['TERMEK_NEV'];
 	}
 
 	oci_free_statement($stid);
