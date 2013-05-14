641fd12f21a261bbd1a5db05d0282a47df124208
bertalan.verebelyi <bertalan.verebelyi@gmail.com>
2013-05-14 11:01:46 +0000

    bejelentkezes-menu.php

---
+++ project/src/netshop/template/bejelentkezes-menu.php
@@ -0,0 +1,31 @@
+<?php
+	require_once '/php/felhasznalo.php';
+	
+	if (logged_in()):
+?>
+<div id="bejelentkezes" class="headerbar-form pull-right">
+	<ul>
+		<li>
+			<a href="login.php">Üdvözlünk <?php echo $_SESSION['nev']; ?></a>
+		</li>
+		<li>
+			<a href="logout.php">Kijelentkezés</a>
+		</li>
+	</ul>
+</div>
+
+<?php else : ?>
+<div id="bejelentkezes" class="headerbar-form pull-right">
+	<ul>
+		<li>
+			<a href="login.php">Bejelentkezés felhasználóként</a>
+		</li>
+		<li>
+			<a href="admin/login.php">Bejelentkezés adminként</a>
+		</li>
+		<li>
+			<a href="registration.php">Regisztráció</a>
+		</li>
+	</ul>
+</div>
+<?php endif; ?>
