c082210c73f9105d5d53f7ca5c736b164eadd93f
csaba.verebelyi@gmail.com <csaba.verebelyi@gmail.com@8c8f8b9a-9680-e8bb-6d29-ba458c0c4fac>
2013-05-14 22:48:23 +0000

    hasonló termékek kilistázása a termék profil oldalon kategória alapján

--- trunk/project/src/netshop/php/products.php
+++ trunk/project/src/netshop/php/products.php
@@ -74,4 +74,77 @@ function product_list(){
                
 }
 
+/*
+ * Hasonló, egy kategóriába tartozó termékek kilistázása
+ */
+function list_similar($id){
+	global $connect;
+	// termékek lekérdezése
+	$stid = oci_parse($connect, "SELECT termek_id, termek_nev FROM termek WHERE kategoria_id = '$id' ORDER BY termek_nev ASC");
+	if (!$stid) {
+		$e = oci_error($connect);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+	// Lekérdezés
+	$r = oci_execute($stid);
+	if (!$r) {
+		$e = oci_error($stid);
+		trigger_error(htmlentites($e['message'], ENT_QUOTES), E_USER_ERROR);
+
+	}
+	print '<ul id="similar">';
+	
+	while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+		print "<li>\n";
+			print '<a href="/netshop/product-profil.php?pid=' . $row['TERMEK_ID'] . '">' . $row['TERMEK_NEV'] . '</a>';
+		print "</li>\n";
+	}
+	print "</ul>\n";
+
+	oci_free_statement($stid);
+	oci_close($connect);			
+               
+}
+
+/*
+ * Termékekre való keresést megvalósító függvény
+ */
+function product_search(){
+	global $connect;
+	// termékek lekérdezése
+	$stid = oci_parse($connect, 'SELECT termek_id, termek_nev, ar FROM termek');
+	if (!$stid) {
+		$e = oci_error($connect);
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+	// Lekérdezés
+	$r = oci_execute($stid);
+	if (!$r) {
+		$e = oci_error($stid);
+		trigger_error(htmlentites($e['message'], ENT_QUOTES), E_USER_ERROR);
+
+	}
+	print "<table>\n";
+	print "<tr>\n";
+	print "<th>ID</th>\n";
+	print "<th>Termék név</th>\n";
+	print "<th>Ár</th>\n";
+	print "<th>Művelet</th>\n";
+	print "</tr>\n";
+	
+	while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+		print "<tr>\n";
+			print "<td>" . $row['TERMEK_ID'] . "</td>";
+			print "<td>" . iconv("ISO-8859-1", "UTF-8", $row['TERMEK_NEV']) . "</td>";
+			print "<td>" . $row['AR'] . "</td>";
+			print "<td>Szerkesztés</td>";
+		print "</tr>\n";
+	}
+	print "</table>\n";
+
+	oci_free_statement($stid);
+	oci_close($connect);			
+               
+}
+
 ?>
